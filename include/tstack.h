727bfb65e2f5859ece2213e9f692eb689a2a9159
HoJLter-study <suv.ioan2000@gmail.com>
2026-04-03 18:22:11 +0300

    Add: infx2pstfx func

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,51 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#define SIZE 100
 
-template<typename T, int size>
+template<typename T>
 class TStack {
-  // добавьте код стека
+private:
+    T stack[SIZE];
+    int topIndex;    
+
+public:
+    TStack() : topIndex(-1) {}
+
+    int push(T value) {
+        if (topIndex >= SIZE - 1) {
+            throw std::runtime_error("Stack overflow!");
+        }
+        stack[++topIndex] = value;
+        return 0;
+    }
+
+    T pop() {
+        if (isEmpty()) {
+            throw std::runtime_error("Stack is empty!!");
+        }
+        return stack[topIndex--];
+    }
+
+    T top() const {
+        if (isEmpty()) {
+            throw std::runtime_error("Stack is empty!");
+        }
+        return stack[topIndex];
+    }
+
+    
+    bool isEmpty() const {
+        return topIndex == -1;
+    }
+
+    bool isFull() const {4
+        return topIndex == SIZE - 1;
+    }
+
+    int getSize() const {
+        return topIndex + 1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,84 @@
 #include <string>
 #include <map>
 #include "tstack.h"
+#include <iostream>
+#include <string>
+#include <map>
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+bool isDigit(char sym) {
+	return sym >= 48 && sym <= 57;
+}
+
+bool isOperator(char sym) {
+	return sym == '+' || sym == '-' ||
+		sym == '*' || sym == '/' ||
+		sym == '(' || sym == ')';
+}
+
+uint8_t getPrior(char oper) {
+	std::map<char, uint8_t> prior = { 
+		{'(', 0}, {')', 1},
+		{'+', 2}, {'-', 2},
+		{'*', 3}, {'/', 3},
+	};
+
+	return prior[oper];
+}
+
+
+std::string infx2pstfx(const std::string& inf) { //(2-1)*(6+2)
+	std::string output;
+	TStack<char> operations;
+	for (char sym : inf) {
+		if (isDigit(sym)) {
+			output += sym;
+		}
+		else if (isOperator(sym)) {
+			if (sym == '(') operations.push(sym);
+			else if (sym == ')') {
+				while (!operations.isEmpty() && operations.top() != '(') {
+					output += operations.pop();
+				}
+				operations.pop();
+			}
+			else if (operations.isEmpty() || sym == '(' ||
+				getPrior(sym) > getPrior(operations.top())) {
+				operations.push(sym);
+			}
+			else if (getPrior(sym) <= getPrior(operations.top())) {
+				while (!operations.isEmpty() &&
+					operations.top() != '(' &&
+					getPrior(sym) <= getPrior(operations.top())) {
+					output += operations.pop();
+				}
+
+				operations.push(sym);
+			}
+
+		}
+	}
+
+	while (!operations.isEmpty()) {
+		output += operations.pop();
+	}
+
+	std::cout << output;
+	return std::string("");
 }
 
 int eval(const std::string& pref) {
-  // добавьте код
+  
   return 0;
+}
+
+
+int main() {
+	infx2pstfx("(2-1)*(6+2)");
+
+	try {
+
+	}
+	catch (const std::runtime_error& e) {
+		std::cout << e.what();
+	}
 }
